763182a4f69152a6519de816cccabdfb8afa8659
ObiDev <marcinobi@gmail.com>
2022-08-12 12:33:52 +0200

    Add project files.

---
+++ src/Shared/Confab.Shared.Infrastructure/Extensions.cs
@@ -0,0 +1,43 @@
+﻿using Confab.Shared.Infrastructure.Api;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: InternalsVisibleTo("Confab.Bootstrapper")]
+namespace Confab.Shared.Infrastructure
+{
+    internal static class Extensions
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            services.AddControllers()
+                .ConfigureApplicationPartManager(manager =>
+                {
+                    manager.FeatureProviders.Add(new InternalControllerFeatureProvider());
+                });
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseInfrastructure(this IApplicationBuilder app)
+        {
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Confab API!");
+                });
+            });
+
+            return app;
+        }
+    }
+}
